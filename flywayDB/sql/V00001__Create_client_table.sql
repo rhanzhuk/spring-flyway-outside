10423fe8ca6827aedf1a6bb40b44fd2bb73c7374
rhanzhuk <hanzhuk_ruslan@icloud.com>
2022-06-20 00:45:07 +0300

    fix v0001 script

--- flywayDB/sql/V00001__Create_client_table.sql
+++ flywayDB/sql/V00001__Create_client_table.sql
@@ -4,4 +4,3 @@ CREATE TABLE client (id INT, name VARCHAR(30), surname VARCHAR(30),age INT, mail
 INSERT INTO client VALUES (1, 'Ben', 'Afleck', 44, 'ben@koresh.com');
 INSERT INTO client VALUES (2, 'Jonny', 'Deph', 50, 'jeck.vorobey@normpac.com');
 INSERT INTO client VALUES (3, 'Shakira', 'Ivanovna', 40, 'shakira@barcelona.otstoy');
-INSERT INTO client VALUES (4, 'TEST', 'TEST', 00, 'TEST');
