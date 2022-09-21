53f14587648f6668c5349428d80b0c2cbef1f069
rhanzhuk <hanzhuk_ruslan@icloud.com>
2022-09-21 22:48:31 +0300

    rm sink

--- setuper/entrypoint.sh
+++ setuper/entrypoint.sh
@@ -9,12 +9,12 @@ auto_auth {
            role = "universal"
        }
    }
-   sink "file" {
-       wrap_ttl = "2m"
-       config = {
-           path = "/app/vault-token-via-agent"
-       }
-   }
+#   sink "file" {
+#       wrap_ttl = "2m"
+#       config = {
+#           path = "/app/vault-token-via-agent"
+#       }
+#   }
 }
 template {
     destination = "/app/config/application.properties"
@@ -23,8 +23,5 @@ template {
 EOF
 
 /app/vault agent -config /app/vault-agent.hcl
-#VAULT_TOKEN=$(/app/vault unwrap -field=token $(jq -r '.token' /app/vault-token-via-agent))
 cat /app/config/application.properties
-#rm -f /app/vault-token-via-agent
-#exec /app/envconsul --no-prefix=true -upcase -sanitize -once -vault-renew-token=false -vault-addr=${VAULT_ADDR} -vault-token=${VAULT_TOKEN} -secret secret/data/app-conf/spring-flyway-local/application.properties "$@"
 exec "$@"
